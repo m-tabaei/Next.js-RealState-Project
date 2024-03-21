b4f35c126ff316d10aef16d440b8e53ed31f5612
mtabaie <mtabaie@gmail.com>
2024-03-21 22:52:27 +0330

    update

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -3,33 +3,37 @@ import { FaCity } from "react-icons/fa";
 import CategoryCard from "@/module/CategoryCard";
 import { categories, cities, services } from "@/constants/strings";
 import styles from "@/template/HomePage.module.css";
+import Slider from "./Slider";
 
 function HomePage() {
   return (
-    <div>
-      <div className={styles.banner}>
-        <div className={styles.desc}>
-          <h1>Purchase and Rental </h1>
+        <main className="d-">
+    <Slider/>
+   </main>
+    // <div>
+    //   <div className={styles.banner}>
+    //     <div className={styles.desc}>
+    //       <h1>Purchase and Rental </h1>
          
-        </div>
-      </div>
-      <div className={styles.categories}>
-        {Object.keys(categories).map((i) => (
-          <CategoryCard key={i} title={categories[i]} name={i} />
-        ))}
-      </div>
-      <div className={styles.city}>
-        <h3>Locations</h3>
-        <ul>
-          {cities.map((i) => (
-            <li key={i}>
-              <FaCity />
-              <span>{i}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    //     </div>
+    //   </div>
+    //   <div className={styles.categories}>
+    //     {Object.keys(categories).map((i) => (
+    //       <CategoryCard key={i} title={categories[i]} name={i} />
+    //     ))}
+    //   </div>
+    //   <div className={styles.city}>
+    //     <h3>Locations</h3>
+    //     <ul>
+    //       {cities.map((i) => (
+    //         <li key={i}>
+    //           <FaCity />
+    //           <span>{i}</span>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
   );
 }
 

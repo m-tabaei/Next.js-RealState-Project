550af5c76ee08a9e50b087719565f609a4881013
mtabaie <mtabaie@gmail.com>
2024-03-23 23:52:23 +0330

    update

--- src/components/template/Slider.js
+++ src/components/template/Slider.js
@@ -1,54 +1,48 @@
 "use client";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import SliderDescription from "./SliderDescription";
 import { images } from "@/constants/sliderPics";
+import style from "@/template/Slider.module.css"
 
 const Slider = () => {
   const [activeImage, setActiveImage] = useState(0);
 
   const clickNext = () => {
-    activeImage === images.length - 1
-      ? setActiveImage(0)
-      : setActiveImage(activeImage + 1);
+    setActiveImage((prevActive) =>
+      prevActive === images.length - 1 ? 0 : prevActive + 1
+    );
   };
+
   const clickPrev = () => {
-    activeImage === 0
-      ? setActiveImage(images.length - 1)
-      : setActiveImage(activeImage - 1);
+    setActiveImage((prevActive) =>
+      prevActive === 0 ? images.length - 1 : prevActive - 1
+    );
   };
 
   useEffect(() => {
     const timer = setTimeout(() => {
       clickNext();
     }, 5000);
-    return () => {
-      clearTimeout(timer);
-    };
+    return () => clearTimeout(timer);
   }, [activeImage]);
+
   return (
-    <main className="grid place-items-center md:grid-cols-2 grid-cols-1 w-full mx-auto max-w-5xl shadow-2xl rounded-2xl">
-      <div
-        className={`w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500 md:rounded-2xl p-6 md:p-0`}
-      >
+    <main className={style.main}>
+      <div className={style.imagecontainer}>
         {images.map((elem, id) => (
-          <div
-            key={id}
-            className={`${
-              id === activeImage
-                ? "block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out"
-                : "hidden"
-            }`}
-          >
-            <Image
-              src={elem.src}
-              alt=""
-              width={400}
-              height={400}
-              className="w-full h-full object-cover md:rounded-tl-3xl md:rounded-bl-3xl"
-            />
-          </div>
+        <div
+        key={id}
+        style={{ display: id === activeImage ? 'block' : 'none' }}
+      >
+        <Image
+          src={elem.src}
+          alt=""
+          width={400}
+          height={400}
+          className={style.image}
+        />
+      </div>
         ))}
       </div>
       <SliderDescription
--- src/components/template/SliderDescription.js
+++ src/components/template/SliderDescription.js
@@ -4,62 +4,42 @@ import right from "../../../public/images/right.svg";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { images } from "@/constants/sliderPics";
-
-
-
+import style from "@/template/SliderDescription.module.css"
 const SliderDescription = ({ activeImage, clickNext, clickPrev }) => {
   return (
-    <div className="grid place-items-start w-full bg-[#e7dfd9] relative md:rounded-tr-3xl md:rounded-br-3xl">
-      <div className="uppercase text-sm absolute right-4 top-2 underline-offset-4 underline">
-        Coffee Cafe
-      </div>
-      {images.map((elem, idx) => (
+    <div className={style.sliderdescriptioncontainer}>
+      <div className={style.coffeecafelabel}>Coffee Cafe</div>
+      {images.map((elem, id) => (
         <div
-          key={idx}
-          className={`${
-            idx === activeImage
-              ? "block w-full h-full md:h-[80vh] py-20 md:px-20 px-10 text-left"
-              : "hidden"
-          }`}
+          key={id}
+          style={{ display: id === activeImage ? 'block' : 'none' }}
+          className={style.slidercontent}
         >
           <motion.div
             initial={{
-              opacity: idx === activeImage ? 0 : 0.5,
-              scale: idx === activeImage ? 0.5 : 0.3,
+              opacity: id === activeImage ? 0 : 0.5,
+              scale: id === activeImage ? 0.5 : 0.3,
             }}
             animate={{
-              opacity: idx === activeImage ? 1 : 0.5,
-              scale: idx === activeImage ? 1 : 0.3,
+              opacity: id === activeImage ? 1 : 0.5,
+              scale: id === activeImage ? 1 : 0.3,
             }}
             transition={{
               ease: "linear",
               duration: 2,
               x: { duration: 1 },
             }}
-            className="w-full"
           >
-            <div className="py-16 text-5xl font-extrabold">{elem.title}</div>
-            <div className="leading-relaxed font-medium text-base tracking-wide h-60 md:h-40 italic text-gray-600">
-              {" "}
-              {elem.desc}
-            </div>
+          <div className={style.title}>{elem.title}</div>
+<div className={style.description}>{elem.desc}</div>
           </motion.div>
 
-          <button className="bg-[#ecae7e] text-white uppercase px-4 py-2 rounded-md my-10">
-            order now
-          </button>
-          <div className="absolute md:bottom-1 bottom-10 right-10 md:right-0 w-full flex justify-center items-center">
-            <div
-              className="absolute bottom-2 right-10 cursor-pointer"
-              onClick={clickPrev}
-            >
+          <button onClick={clickNext}>order now</button>
+          <div className={style.slidercontrols}>
+            <div className={style.leftarrow} onClick={clickPrev}>
               <Image src={left} alt="" />
             </div>
-
-            <div
-              className="absolute bottom-2 right-2 cursor-pointer"
-              onClick={clickNext}
-            >
+            <div className={style.rightarrow} onClick={clickNext}>
               <Image src={right} alt="" />
             </div>
           </div>
